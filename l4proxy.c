9af93f771651096f565534eeb07a7d62f8a0a28e
Artem Yazkov <artem.yazkov@gmail.com>
2024-07-08 11:48:19 +0500

    finalize proxy_loop_do() implementation

--- l4proxy.c
+++ l4proxy.c
@@ -2,12 +2,17 @@
 #include <fcntl.h>
 #include <poll.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <linux/sockios.h>
 
-#define MAX_PREFIX_SIZE     16
-#define MAX_DGRAM_SIZE      128
+#define MAX_PREFIX_SZ_MAX    16
+#define MAX_DGRAM_SZ_MAX    128
+#define MAX_DGRAM_SZ_MIN     16
 
 typedef struct cmdopts_s {
     struct in_addr  dw_addr;
@@ -26,6 +31,9 @@ int main(int argc, char **argv)
     cmdopts_t opts = (cmdopts_t){0};
     inet_aton("0.0.0.0", &opts.dw_addr);
     opts.dw_port = htons((uint16_t)8080);
+
+    inet_aton("192.168.31.146", &opts.up_addr);
+    opts.up_port = htons((uint16_t)1234);
 
     proxy_loop_do(&opts);
 
@@ -67,17 +75,19 @@ int proxy_loop_do(cmdopts_t *opts)
         .sin_port = opts->up_port
     };
 
+    /* Event Loop initialization */
+    enum upstate_e {
+        UPSTATE_INIT,
+        UPSTATE_CONNECTING,
+        UPSTATE_CONNECTED,
+        UPSTATE_HEARTSINK
+    } up_state = UPSTATE_INIT;
+    struct timespec tm_heartbeat = {.tv_sec = 1};
+    struct timespec tm_heartsink = {0};
+
     for (;;) {
-        char msg[MAX_PREFIX_SIZE + MAX_DGRAM_SIZE];
-        int  msg_l = 0; /* actual length of message data */
-        int  msg_c = 0; /* message cursor allow us to send the message partially */
-
-        int  up_is_alive = 0; /* 1 if upstream socket connection is alive */
-        int  up_buff_s   = 0; /* upstream socket buffer total size        */
-        int  up_buff_l   = 0; /* upstream socket buffer total size        */
-
-        /* first of all, care about UP side connection */
-        if (up_pfd->fd < 0) {
+        /* care about UP side (re)connection */
+        if (up_state == UPSTATE_INIT) {
             if ((up_pfd->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
                 goto error;
             }
@@ -88,57 +98,103 @@ int proxy_loop_do(cmdopts_t *opts)
             if (setsockopt(up_pfd->fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(sockopt)) < 0) {
                 goto error;
             }
-            /* SOCK_DGRAM nature allow us do not care about messages split/re-assembly even on NONBLOCK mode
-             * however, for SOCK_STREAM we also want to make our message-sent operations atomic
-             *
-             * unfortunately we can't adjust SO_SNDLOWAT option on Linux,
-             * but can rely on SO_SNDBUF-SIOCOUTQ (total SNDBUFF - filled SNDBUFF) difference */
-            socklen_t sockopt_len = sizeof(up_buff_s);
-            if (getsockopt(up_pfd->fd, SOL_SOCKET, SO_SNDBUF, &up_buff_s, &sockopt_len) < 0) {
-                goto error;
-            }
-            printf("up_buff_s: %d bytes\n", up_buff_s);
-
-
             int ccode = connect(up_pfd->fd, (struct sockaddr *)(&up_sain), sizeof(up_sain));
             if ((ccode < 0) && (errno != EINPROGRESS)) {
                 goto error;
             }
-            if (ccode < 0) {
-                /* connection in progress. Wait for POLLOUT */
-                up_pfd->events = POLLOUT;
-            } else {
-                /* immediately connected */
-                up_is_alive = 1;
-            }
+            up_pfd->events = POLLOUT;
+            up_state = UPSTATE_CONNECTING;
         }
 
-        int pcode = ppoll(pfds, 2, NULL, NULL);
+        /* take UP/DOWN events */
+        int pcode = ppoll(pfds, 2, &tm_heartbeat, NULL);
         if (pcode < 0) {
             goto error;
         }
-
-        if (up_pfd->revents & POLLOUT) {
-            up_is_alive = 1;
-            up_pfd->events = 0; /* we want one-shot POLLOUT event */
+        if (up_state == UPSTATE_HEARTSINK) {
+            /* HEARTSINK is an intermediate state between CONNECTED and INIT
+             * adds the necessary pause for UP side reconnect and eliminate
+             * reconnection flood on local sockets
+             *
+             * DOWN side events still processed without delays */
+            struct timespec tm_cur;
+            if (clock_gettime(CLOCK_REALTIME, &tm_cur) < 0) {
+                goto error;
+            }
+            uint64_t tm64_cur = tm_cur.tv_sec * (uint64_t)1000000000L + tm_cur.tv_nsec;
+            uint64_t tm64_hts = tm_heartsink.tv_sec * (uint64_t)1000000000L + tm_heartsink.tv_nsec;
+            uint64_t tm64_htb = tm_heartbeat.tv_sec * (uint64_t)1000000000L + tm_heartbeat.tv_nsec;
+            if ((tm64_cur - tm64_hts) >= tm64_htb) {
+                up_state = UPSTATE_INIT;
+            }
         }
+
         if ((up_pfd->revents & POLLHUP) || (up_pfd->revents & POLLERR)) {
-            up_is_alive = 0;
+            /* error on UP side socket was happen */
             close(up_pfd->fd);
-            up_pfd->fd = -1;
+            up_pfd->fd = -1;  /* no more UP side events until reinitialize */
+            if (clock_gettime(CLOCK_REALTIME, &tm_heartsink) < 0) {
+                goto error;
+            }
+            up_state = UPSTATE_HEARTSINK;
+        } else if (up_pfd->revents & POLLOUT) {
+            /* UP side socket is good */
+            up_state = UPSTATE_CONNECTED;
+            up_pfd->events = 0; /* stop POLLOUT flood */
         }
 
         if (dw_pfd->revents & POLLIN) {
             struct sockaddr  dw_src;
             socklen_t        dw_src_l;
+            char             msg[MAX_PREFIX_SZ_MAX + MAX_DGRAM_SZ_MAX];
+            int              msg_dt = 0;
+
+            /* SOCK_DGRAM nature allow us do not care about messages split/re-assembly even on NONBLOCK mode
+             * for SOCK_STREAM we also want to make our message-sent operations atomic
+             * (otherwise we need to save up incoming UDP messages that goes against the task requirements)
+             *
+             * so lets sent only if we have enough space in socket buffer for entire message;
+             * discard the message otherwise */
+
+            int upbuf_sz = 0;  /* UP side socket buffer total size */
+            int upbuf_dt = 0;  /* UP side socket buffer data size  */
+            int upbuf_fr = 0;  /* UP side socket buffer free size  */
+            if (up_state == UPSTATE_CONNECTED) {
+                socklen_t upbuf_sz_le = sizeof(upbuf_sz);
+                if (getsockopt(up_pfd->fd, SOL_SOCKET, SO_SNDBUF, &upbuf_sz, &upbuf_sz_le) < 0) {
+                    goto error;
+                }
+                if (ioctl(up_pfd->fd, SIOCOUTQ, &upbuf_dt) < 0) {
+                    goto error;
+                }
+                upbuf_fr = (upbuf_sz > upbuf_dt) ? (upbuf_sz - upbuf_dt) : 0;
+                if (!upbuf_fr) {
+                    continue;
+                }
+                printf("upbuf_sz: %d, upbuf_dt: %d, upbuf_fr: %d\n", upbuf_sz, upbuf_dt, upbuf_fr);
+            }
 
             /* we expect strictly one full-filled message per receive call or nothing
              * zero-sized messages are possible */
-            while ((msg_l = recvfrom(dw_pfd->fd, msg, MAX_DGRAM_SIZE, 0, &dw_src, &dw_src_l)) >= 0) {
-                if (up_is_alive == 0) {
-                    /* just skip the message */
+            while ((msg_dt = recvfrom(dw_pfd->fd, &msg[opts->prefix_len], MAX_DGRAM_SZ_MAX, 0, &dw_src, &dw_src_l)) >= 0) {
+                if ((up_state == UPSTATE_CONNECTED) &&
+                    //(msg_dt >= MAX_DGRAM_SZ_MIN) &&
+                    (msg_dt < upbuf_fr)
+                ) {
+                    printf("forward %d bytes message: '%.*s'\n", msg_dt, msg_dt, &msg[opts->prefix_len]);
+                    int scode = send(up_pfd->fd, msg, opts->prefix_len + msg_dt, 0);
+                    if (scode != opts->prefix_len + msg_dt) {
+                        /* unexpected state; close UP socket
+                         * socket state will fall into UPSTATE_HEARTSINK on POLLHUP event */
+                        close(up_pfd->fd);
+                        break;
+                    } else {
+                        upbuf_fr -= scode;
+                        printf("was sent successfully\n");
+                    }
                 } else {
-                    printf("forward %d bytes to upstream: '%.*s'\n", msg_l, msg_l, msg);
+                    printf("discard %d bytes message: '%.*s'\n", msg_dt, msg_dt, &msg[opts->prefix_len]);
+                    /* just discard the message */
                 }
             }
 
@@ -154,6 +210,7 @@ int proxy_loop_do(cmdopts_t *opts)
 
 error:
     retcode = -1;
+
 finalize:
     if (dw_pfd->fd >= 0) {
         close(dw_pfd->fd);
